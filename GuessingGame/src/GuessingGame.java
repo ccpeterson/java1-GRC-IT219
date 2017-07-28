b729f030c074ba630d1718db7b6c62ed92c5264c
ccpeterson <clinton.c.peterson@gmail.com>
2017-07-27 17:59:29 -0700

    guess

--- GuessingGame/src/GuessingGame.java
+++ GuessingGame/src/GuessingGame.java
@@ -1,39 +1,38 @@
+// header goes here
+
 import acm.program.*;
 import acm.util.RandomGenerator;
-
-import java.awt.Color;
-import java.awt.event.*;
-
 public class GuessingGame extends ConsoleProgram
 {
+	//windows size constants
 	final int WIDTH = 800;
-	final int HEIGHT = 500;
-	//create a random number generator name rg
-	RandomGenerator rg = new RandomGenerator();
+	final int HEIGHT = 500;	
 	//create a constants for the array size, range of numbers, and number of guesses	
 	final int ARRAY_SIZE = 5;
 	final int MIN_RANGE = 1;
 	final int MAX_RANGE = 20;
-	final int GUESS_ATTEMPTS = 3;
+	final int GUESS_ATTEMPTS = 3;	
+	//create a random number generator name rg
+	RandomGenerator rg = new RandomGenerator();	
 	//declare variables so they can be used globally
 	int answers[] = new int[ARRAY_SIZE] ;
 	int guess;
-	boolean winner = false;
-	
+	boolean winner = false;	
+	//init so we can resize the window
 	public void init()
 	{
 		setSize(WIDTH, HEIGHT);
-		setBackground(Color.BLACK);
-	}
-	
+	}		
 	public void run() {
 		//setup the array with random numbers
-		setupAnswers();
+		setupAnswers();		
 		//this prints the array so we can see the answers while we create it
+		/*
 		for(int i=0; i < ARRAY_SIZE; i++)
-		{
+		//{
 			println(answers[i]);
 		}
+		*/		
 		//welcomes users and explains the game
 		println("Hey there, this is a guessing game.");
 		println("I have thought of " + ARRAY_SIZE + " numbers between (and including) " + MIN_RANGE + " and " + MAX_RANGE + ".");
@@ -60,16 +59,14 @@ public class GuessingGame extends ConsoleProgram
 		else {
 			println("You are out of guesses, too bad.");
 		}
-	}
-	
+	}	
 	//the fills the array with random numbers
 	public void setupAnswers() {
 		for(int i=0; i < ARRAY_SIZE; i++)
 		{
 			answers[i] = getRandomPick();
 		}
-	}
-	
+	}	
 	// a method to pick a random number within the range specified
 	public int getRandomPick() {
 		return rg.nextInt(MIN_RANGE,MAX_RANGE);		
@@ -77,12 +74,10 @@ public class GuessingGame extends ConsoleProgram
 	// a method to ensure the guesses are within the range they are supposed to be
 	// will return the number if it's valid, otherwise will loop until the users picks a good one 
 	public int checkInput() {
-		while (true){
-			
-			
-			
+		while (true){			
+			// readInt already makes sure the input is a valid int without crashing
 			int x = readInt("What's your guess: ");
-			
+			//this if makes sure the number picked is within the right range
 			if (x>=MIN_RANGE && x <= MAX_RANGE){
 				return x;				
 			}
