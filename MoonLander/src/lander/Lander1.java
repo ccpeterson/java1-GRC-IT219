ea88ce1929556ec581eadc2367047ae2766b5f5e
ccpeterson <clinton.c.peterson@gmail.com>
2017-08-08 19:02:57 -0700

    lander work

--- MoonLander/src/lander/Lander1.java
+++ MoonLander/src/lander/Lander1.java
@@ -16,49 +16,72 @@ public class Lander1 extends GraphicsProgram {
 	public static final int REFRESH_RATE = 50;
 	//upward speed added every click
 	public static final int THRUST = 10;
-	public GImage lander;
-	public int rateOfDecent = 0;
 	
-		
+	public GImage lander;
+	public GRect target;
+	public int rateOfDecent = 0;
+			
 	public void init() {
 		setSize(WIN_WIDTH, WIN_HEIGHT);
 		setBackground(Color.WHITE);
-	}
-	
+	}	
 	public void run() {
-		addLander();
+		setupGame();
 		printStartGame();
 		playGame();
-	}
-	
+		printGameOver();
+	}	
+	public void setupGame() {
+		addTarget();
+		addLander();
+	}	
 	public void addLander() {
 		lander = new GImage("lander.JPG");
 		lander.setBounds(WIN_WIDTH/2, 100, 100, 75);
 		add(lander);		
-	}
-	
+	}	
+	public void addTarget() {
+		target = new GRect(WIN_WIDTH/2,WIN_HEIGHT-50,200,20);
+		target.setFillColor(Color.GRAY);
+		target.setFilled(true);
+		add(target);
+	}	
 	public void mouseMoved(MouseEvent e) {
 		if ((e.getX() < getWidth() ) && (e.getX() > 0)) {
 			lander.setLocation(e.getX() , lander.getY());
 		}		
-	}
-	
+	}	
 	public void mousePressed(MouseEvent e) {
 		rateOfDecent = rateOfDecent - THRUST;
-	}
-	
-	public void startGravity() {
-		while (true) {
-			lander.move(0, rateOfDecent);
-			rateOfDecent = rateOfDecent + GRAVITY;
-			pause(REFRESH_RATE);
-		}
-	}
-	
+	}	
 	public void playGame() {
 		waitForClick();
 		addMouseListeners();
 		startGravity();		
+	}	
+	public void startGravity() {
+		while (true) {
+			lander.move(0, rateOfDecent);
+			if (checkCollision()==true) {
+				printWinner();
+				break;
+			}
+			rateOfDecent = rateOfDecent + GRAVITY;
+			pause(REFRESH_RATE);
+		}
+	}
+	public boolean checkCollision() {
+		if (lander.getY()+lander.getHeight() >= target.getY()) {
+			if (lander.getX() > target.getX() && lander.getX()+lander.getWidth() < target.getX() + target.getWidth()) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		else {
+			return false;
+		}
 	}
 	
 	public void printStartGame() {
@@ -68,18 +91,15 @@ public class Lander1 extends GraphicsProgram {
 		add (startGame);
 		waitForClick();
 		remove (startGame);
-	}
-	
+	}	
 	public void printGameOver() {
 		GLabel gameOver = new GLabel ("Game Over", getWidth()/2, getHeight()/2);
 		gameOver.setColor(Color.RED);
 		add (gameOver);
-	}	
-	
+	}
 	public void printWinner() {
 		GLabel Winner = new GLabel ("Winner!!", getWidth()/2, getHeight()/2);
 		Winner.setColor(Color.RED);
 		add (Winner);
 	}
-
 }
