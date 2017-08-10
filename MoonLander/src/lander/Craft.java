a898ea6da7110f9c42e3b2e4b14d27ed2f1059e5
ccpeterson <clinton.c.peterson@gmail.com>
2017-08-10 15:56:13 -0700

    classes suck

---
+++ MoonLander/src/lander/Craft.java
@@ -0,0 +1,16 @@
+package lander;
+
+import java.awt.Color;
+import java.awt.event.*;
+import java.util.Random;
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Craft {
+	
+	String name;
+	
+	public Craft(String name) {
+		this.name = name;
+	}
+}
